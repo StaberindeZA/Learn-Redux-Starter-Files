682bfcbdbf911a49d6372c0b867c9a1e9a49b3ba
Reino Muhl <reino.muhl@gmail.com>
2017-06-14 19:45:51 -0400

    Minor changes

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -6,6 +6,24 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // Import Components
-import Main from './components/Main.js';
+import App from './components/App.js';
+import Single from './components/Single.js';
+import PhotoGrid from './components/PhotoGrid.js';
 
-render(<Main/>, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="/view/:postId" component={Single}></Route>
+			</Route>
+		</Router>
+	</Provider>
+)
+
+render(router, document.getElementById('root'));
